b33293f2cf36d57caea31fb948f2088c85eb5919
takashabe <takashabe.public@gmail.com>
2017-07-14 19:03:04 +0900

    Support some protocol in CLI

--- cli.go
+++ cli.go
@@ -1,9 +1,11 @@
 package printserver
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"io"
+	"time"
 )
 
 // Exit codes. used only in Run()
@@ -14,6 +16,7 @@ const (
 	ExitCodeError = 10 + iota
 	ExitCodeParseError
 	ExitCodeInvalidArgsError
+	ExitCodeListenError
 )
 
 // default parameters
@@ -23,8 +26,8 @@ const (
 )
 
 type param struct {
-	proto string
-	addr  string
+	protocol string
+	addr     string
 }
 
 // CLI is the command line interface object
@@ -43,14 +46,41 @@ func (c *CLI) Run(args []string) int {
 		return ExitCodeParseError
 	}
 
-	switch param.proto {
-	case "http", "tcp":
-		fmt.Fprintf(c.ErrStream, "yet not support protocol")
+	var printer Printer
+
+	switch param.protocol {
+	case "http":
+		printer = &PrintHTTP{
+			outStream: c.OutStream,
+			errStream: c.ErrStream,
+			localAddr: make(chan string, 1),
+		}
+	case "tcp":
+		printer = &PrintTCP{
+			outStream: c.OutStream,
+			errStream: c.ErrStream,
+			interval:  time.Second,
+			localAddr: make(chan string, 1),
+		}
 	case "udp":
-		runUDP(param.addr)
+		printer = &PrintUDP{
+			outStream: c.OutStream,
+			errStream: c.ErrStream,
+			interval:  time.Second,
+			localAddr: make(chan string, 1),
+		}
 	default:
 		fmt.Fprintf(c.ErrStream, "unknown protocol")
 		return ExitCodeInvalidArgsError
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := printer.Listen(ctx, param.addr)
+	if err != nil {
+		fmt.Fprintf(c.ErrStream, "failed to Listen at %s protocol", printer.Protocol())
+		return ExitCodeListenError
 	}
 
 	return ExitCodeOK
@@ -60,7 +90,7 @@ func (c *CLI) parseArgs(args []string, p *param) error {
 	flags := flag.NewFlagSet("param", flag.ContinueOnError)
 	flags.SetOutput(c.ErrStream)
 
-	flags.StringVar(&p.proto, "proto", defaultProto, "Network protocol. By default, http. Choose from http, tcp and udp.")
+	flags.StringVar(&p.protocol, "protocol", defaultProto, "Network protocol. By default, http. Choose from http, tcp and udp.")
 	flags.StringVar(&p.addr, "addr", defaultAddr, "Listen port. By default, localhost:0 (:0 means, return a free port).")
 
 	return flags.Parse(args)
